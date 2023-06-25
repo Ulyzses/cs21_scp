adc70c6e6bee20ad0784c6fc2290f0ee43937fc2
Ulyzses <ulyzses.code@gmail.com>
2023-06-26 02:14:27 +0800

    Initial commit

---
+++ CS21_Project2_SCP.srcs/sources_1/new/testbench.sv
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+
+module testbench();
+
+  logic         clk;
+  logic         reset;
+  
+  logic [31:0]  writedata, dataadr;
+  logic         memwrite;
+  
+  // instantiate device to be tested
+  top dut(clk, reset, writedata, dataadr, memwrite);
+  
+  // initialize test
+  initial begin
+    reset <= 1; #5; reset <= 0;
+  end
+  
+  always begin
+    clk <= 1; #5;
+    clk <= 0; #5;
+  end
+
+endmodule
